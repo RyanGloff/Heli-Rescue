1b8fea41d1c662f03df423dddb8b33fe68a1dbbd
nKowalchyk <nickkowalchyk@gmail.com>
2017-03-01 02:24:32 -0500

    Update Source.cpp

--- Heli-Rescue/Source.cpp
+++ Heli-Rescue/Source.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <SDL.h>
-
 #include "Window.h"
+#include "InputHandler.h"
 
 void init();
 void tick();
 void render();
 void exit();
+void handle(bool& isRunning, Window& window);
 
 const int WINDOW_WIDTH = 1080;
 const int WINDOW_HEIGHT = 720;
 
 Window* window = nullptr;
-
-bool running = true;
-
-SDL_Event e;
 
 int main(int argc, char* argv[]) {
 	init();
@@ -24,12 +21,13 @@ int main(int argc, char* argv[]) {
 	Uint32 startTick = SDL_GetTicks();
 	Uint32 startRender = SDL_GetTicks();
 	int frames = 0;
-	while (running) {
-		while (SDL_PollEvent(&e) != 0) {
-			if (e.type == SDL_QUIT || e.key.keysym.sym == SDLK_ESCAPE) {
-				running = false;
-			}
-		}
+
+	bool isRunning = true;
+
+	while(isRunning)
+	{
+		handle(isRunning, *window);
+	
 		Uint32 currentTick = SDL_GetTicks();
 		if (currentTick - startTick >= TIME_PER_TICK) {
 			tick();
@@ -66,3 +64,8 @@ void exit() {
 	delete window;
 	window = nullptr;
 }
+
+void handle(bool& isRunning, Window& window) {
+	InputHandler::handle(isRunning, window);
+}
+
